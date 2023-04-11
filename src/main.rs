ca914edeba4af9304967e3b16047e3afbdc186dc
caro <carolinaluciamauro@gmail.com>
2023-04-10 22:18:11 -0300

    agrego documentación main

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,11 @@ fn read_file_name(default_file: Option<String>) -> Result<String, String> {
     }
 }
 
+// Obtiene el nombre de un archivo y llama a la función print_move_result() con el nombre del mismo en caso que
+// sea un archivo válido. Sino se imprime el error.
+// Si la función print_move_result() devuelve un resultado válido, se imprime ese resultado (que corresponde al resultado de
+// la jugada contenida en el archivo)
+// Si se produce un error en la función print_move_result(), se imprime un mensaje de error.
 fn main() {
     let file_name = match read_file_name(Some(DEFAULT_FILE.to_string())) {
         Ok(f) => f,
